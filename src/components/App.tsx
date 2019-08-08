c1c2922c6cd5cfc7a3ef8148f59bab9f61588083
Aleksandr <besrezen90@mail.ru>
2019-08-08 09:17:41 +0300

    add eslint prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import style from "./App.less";
+import React, { Component } from 'react';
+import style from './App.less';
 
 interface IProps {}
 interface IState {}
 
 export default class App extends Component<IProps, IState> {
-  render() {
-    return <div className={style.app}>Тестовая сборка для проверки</div>;
-  }
+	render() {
+		return <div className={style.app}>Тестовая сборка для проверки</div>;
+	}
 }
