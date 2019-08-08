ff2fcdbc8e20df24cac8d6ecf39f118775813089
Aleksandr <besrezen90@mail.ru>
2019-08-08 14:30:13 +0300

    test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,11 +5,11 @@ interface IProps {}
 interface IState {}
 
 export default class App extends Component<IProps, IState> {
-	render() {
-		return (
-			<div className={style.app}>
-				Тестовая сборка для проверки
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className={style.app}>
+        Тестовая сборка для проверки
+      </div>
+    );
+  }
 }
